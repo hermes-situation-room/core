8d022a199b1cd27fd322c27d8c7afcc68957c93d
Krieger Dario <dario.krieger@icloud.com>
2025-10-01 09:32:03 -0700

    chore: add about controller

---
+++ src/backend/Hermes.SituationRoom.Web/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+namespace Hermes.SituationRoom.Api.Controllers;
+
+using Hermes.SituationRoom.Api.Configurations;
+using Hermes.SituationRoom.Api.Controllers.Base;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+
+public class AboutController(IControllerInfrastructure controllerInfrastructure) : SituationRoomControllerBase(controllerInfrastructure)
+{
+    [HttpGet("external/about")]
+    [SwaggerOperation(Tags = [SwaggerTagDescriptions.ENDPOINT_TAG_EXTERNAL])]
+    [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(string), StatusCodes.Status500InternalServerError)]
+    public async Task<ActionResult> GetAsync()
+    {
+        return Ok("Hermes Situation Room Api up and running.");
+    }
+}
