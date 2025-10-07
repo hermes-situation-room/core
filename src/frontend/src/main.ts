406a8efda2abae7d15707202c8b62e5520a1dd66
Krieger Dario <dario.krieger@icloud.com>
2025-10-07 13:15:38 -0700

    fix: remove wrong declared using import of vuetify styles

--- src/frontend/src/main.ts
+++ src/frontend/src/main.ts
@@ -3,7 +3,6 @@ import router from "./router";
 import App from "./App.vue";
 import "@mdi/font/css/materialdesignicons.css";
 
-import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
