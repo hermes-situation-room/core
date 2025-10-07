699c44b6f568ff5eddca1bd77e9c7dedcfa5afc8
dario krieger <kriegerdario.eclipseboundaries@gmail.com>
2025-10-07 15:22:04 -0700

    feat: add domain dedicated tags for endpoints (#14)
    Co-authored-by: Krieger Dario <dario.krieger@icloud.com>

--- src/backend/Hermes.SituationRoom.Web/Configurations/SwaggerTagDescriptions.cs
+++ src/backend/Hermes.SituationRoom.Web/Configurations/SwaggerTagDescriptions.cs
@@ -8,6 +8,10 @@ public class SwaggerTagDescriptions : IDocumentFilter
     public const string ENDPOINT_TAG_EXTERNAL = "External";
     public const string ENDPOINT_TAG_INTERNAL = "Internal";
     public const string ENDPOINT_TAG_INTERNAL_CODETABLES = "Codetable";
+    public const string ENDPOINT_TAG_INTERNAL_ACTIVIST = "Internal Activist";
+    public const string ENDPOINT_TAG_INTERNAL_CHAT = "Internal Chat";
+    public const string ENDPOINT_TAG_INTERNAL_JOURNALIST = "Internal Journalist";
+    public const string ENDPOINT_TAG_INTERNAL_USER= "Internal User";
 
     public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
     {
@@ -16,6 +20,10 @@ public class SwaggerTagDescriptions : IDocumentFilter
             new() { Name = ENDPOINT_TAG_EXTERNAL, Description = $"Public-facing endpoints. Scope: \"External\"" },
             new() { Name = ENDPOINT_TAG_INTERNAL, Description = $"Internal endpoints for internal system operations within the SituationRoom system. Scope: \"Internal\"" },
             new() { Name = ENDPOINT_TAG_INTERNAL_CODETABLES, Description = $"Internal endpoints for internal codetable operations within the Customer system. Scope: \"Codetable\"" },
+            new() { Name = ENDPOINT_TAG_INTERNAL_ACTIVIST, Description = $"Internal endpoints for internal system operations within the SituationRoom system. Scope: \"Internal\"" },
+            new() { Name = ENDPOINT_TAG_INTERNAL_CHAT, Description = $"Internal endpoints for internal system operations within the SituationRoom system. Scope: \"Internal\"" },
+            new() { Name = ENDPOINT_TAG_INTERNAL_JOURNALIST, Description = $"Internal endpoints for internal system operations within the SituationRoom system. Scope: \"Internal\"" },
+            new() { Name = ENDPOINT_TAG_INTERNAL_USER, Description = $"Internal endpoints for internal system operations within the SituationRoom system. Scope: \"Internal\"" },
         };
     }
 }
