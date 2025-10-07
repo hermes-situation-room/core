0c9a00129303ebdb92376d8e0990ef74ca3b8fb0
dario krieger <kriegerdario.eclipseboundaries@gmail.com>
2025-10-07 13:25:02 -0700

    feat: add vuetify styles library (#13)
    * feat: add vuetify styles library
    
    * fix: remove wrong declared using import of vuetify styles
    
    ---------
    
    Co-authored-by: Krieger Dario <dario.krieger@icloud.com>

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -2,13 +2,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import App from "../App.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: "/",
-      component: App,
-    },
-  ],
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: "/",
+            component: App,
+        },
+    ],
 });
 
 export default router;
