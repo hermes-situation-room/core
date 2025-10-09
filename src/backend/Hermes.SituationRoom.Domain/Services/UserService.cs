62fad911518a3f3e69e744c8059c94b5b3119643
tomasanic <tomas.anic@kfnmail.ch>
2025-10-10 00:00:39 +0200

    add user profile logic (#25)
    * add user profile logic
    
    * use Bo instead of Entity
    
    * modify userBo logic

--- src/backend/Hermes.SituationRoom.Domain/Services/UserService.cs
+++ src/backend/Hermes.SituationRoom.Domain/Services/UserService.cs
@@ -8,6 +8,8 @@ public class UserService(IUserRepository userRepository) : IUserService
 {
     public Task<UserBo> GetUserAsync(Guid userUid) => userRepository.GetUserBoAsync(userUid);
 
+    public Task<UserBo> GetUserProfileAsync(Guid userUid, Guid consumerUid) => userRepository.GetUserProfileBoAsync(userUid, consumerUid);
+
     public Task<IReadOnlyList<UserBo>> GetUsersAsync() => userRepository.GetAllUserBosAsync();
 
     public Task<Guid> CreateUserAsync(UserBo userBo) => userRepository.AddAsync(userBo);
