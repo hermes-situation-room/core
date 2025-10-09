6789422eb9f10b79b42f686836b0df2994f8e834
Krieger Dario <dario.krieger@icloud.com>
2025-10-09 09:47:05 -0700

    chore: remove unused swagger tag descriptions

--- src/backend/Hermes.SituationRoom.Web/Configurations/SwaggerTagDescriptions.cs
+++ src/backend/Hermes.SituationRoom.Web/Configurations/SwaggerTagDescriptions.cs
@@ -6,8 +6,6 @@ namespace Hermes.SituationRoom.Api.Configurations;
 public class SwaggerTagDescriptions : IDocumentFilter
 {
     public const string ENDPOINT_TAG_EXTERNAL = "External";
-    public const string ENDPOINT_TAG_INTERNAL = "Internal";
-    public const string ENDPOINT_TAG_INTERNAL_CODETABLES = "Codetable";
     public const string ENDPOINT_TAG_INTERNAL_ACTIVIST = "Internal Activist";
     public const string ENDPOINT_TAG_INTERNAL_CHAT = "Internal Chat";
     public const string ENDPOINT_TAG_INTERNAL_POST = "Internal Post";
@@ -23,8 +21,6 @@ public class SwaggerTagDescriptions : IDocumentFilter
         swaggerDoc.Tags = new List<OpenApiTag>
         {
             new() { Name = ENDPOINT_TAG_EXTERNAL, Description = $"Public-facing endpoints. Scope: \"External\"" },
-            new() { Name = ENDPOINT_TAG_INTERNAL, Description = internalScopeDescription },
-            new() { Name = ENDPOINT_TAG_INTERNAL_CODETABLES, Description = $"Internal endpoints for internal codetable operations within the Customer system. Scope: \"Codetable\"" },
             new() { Name = ENDPOINT_TAG_INTERNAL_ACTIVIST, Description = internalScopeDescription },
             new() { Name = ENDPOINT_TAG_INTERNAL_CHAT, Description = internalScopeDescription },
             new() { Name = ENDPOINT_TAG_INTERNAL_POST, Description = internalScopeDescription },
