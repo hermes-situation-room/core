16dd3c44e3ebde353f6376ecb42e834196d98c2a
noahpolgar <polgar_noah@bluewin.ch>
2025-10-11 00:48:59 +0200

    Feat: SocketBaseClient and functionality

---
+++ src/frontend/src/services/api/base/socket-base-client.ts
@@ -0,0 +1,44 @@
+import * as signalR from '@microsoft/signalr';
+
+const apiBaseUrl = import.meta.env.VITE_APP_BACKEND;
+const apiHubPath = import.meta.env.VITE_APP_SOCKET_HUB_PATH;
+
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl(`${apiBaseUrl}${apiHubPath}`, {
+        withCredentials: false
+    })
+    .build();
+
+export default class SocketBaseClient {
+
+    /**
+     * Registers a new event to be triggered with a certain backend socket-message
+     * @param event Event-name to be registered in the backend
+     * @param action Function which is executed with the response-object from the socket-message
+     */
+    registerToEvent = (event: string, action: Function) => {
+        connection.on(event, (response) => action(response));
+        console.log(`BaseClient registered to event: '${event}'`);
+    }
+
+    /**
+     * Send an event over the socket to the backend
+     * @param event Event-name as string which addresses the backend-socket-endpoint
+     * @param args Array of any-types which will be passed to the backend
+     */
+    sendEvent = (event: string, args: any[]) => {
+        connection.send(event, ...args);
+        console.log(`BaseClient sending event '${event}'`);
+    }
+    /**
+     * Initializes the socket-connection and returns the state of the initialization as promise
+     * @returns Promise which represents the initialization-state of the socket-connection 
+     * -> promise.resolved == connection is fully initialized
+     */
+    initialize = (): Promise<void> => {
+        console.log(`BaseClient start socket connection`)
+        const promise = connection.start()
+            .catch((err) => console.log(err))
+        return promise;
+    }
+}
--- src/frontend/src/services/api/index.ts
+++ src/frontend/src/services/api/index.ts
@@ -1,8 +1,50 @@
 import aboutApi from "./about-api";
 import ApiBaseClient from "./base/api-base-client";
+import SocketBaseClient from "./base/socket-base-client";
 
 const apiBaseClient = new ApiBaseClient();
 
 export const services = {
     about: aboutApi(apiBaseClient)
+}
+
+const socketBaseClient = new SocketBaseClient();
+/**
+ * Indicated the initialization-state of the socket. If promise is resolved, the socket is fully initialized
+ */
+const initPromise = socketBaseClient.initialize();
+
+export const sockets = {
+    hub: {
+        /**
+         * Ensures the socket for hub is fully initialized. Always call ensure before registering or sending over the socket 
+         */
+        ensureSocketInitialization: async() => {
+            await initPromise;
+            console.log("Hub-Socket initialized");
+        },
+        /**
+         * Registers a new event over the socket
+         * @param event Sting with the event-name which is triggered over the socket
+         * @param action Function which is executed when the event is triggered over the socket
+         */
+        registerToEvent: async(event: string, action: Function) => {
+            socketBaseClient.registerToEvent(event, action);
+        },
+        /**
+         * Send a JoinChat event over the socket to the backend
+         * @param userId Guid (string) with the userId
+         * @param chatId Guid (string) with the chatId
+         */
+        joinChat: (userId: string, chatId: string) => {
+            socketBaseClient.sendEvent("JoinChat", [userId, chatId]);
+        },
+        /**
+         * Send a LeaveChat event over the socket to the backend to unsubscribe to chat messages
+         * @param chatId Guid (string) with the chatId to be left
+         */
+        leaveChat: (chatId: string) => {
+            socketBaseClient.sendEvent("LeaveChat", [chatId]);
+        }
+    }
 }
