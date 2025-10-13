77f9f293d4ee9e643b280f1a170101c62bb9462d
Lukas Huebscher <lukas.huebscher@roche.com>
2025-10-13 21:13:05 +0200

    Add Bo

---
+++ src/backend/Hermes.SituationRoom.Shared/BusinessObjects/UserChatReadStatusBo.cs
@@ -0,0 +1,10 @@
+﻿#nullable enable
+namespace Hermes.SituationRoom.Shared.BusinessObjects;
+
+public record UserChatReadStatus(Guid UserId,
+    Guid ChatId,
+    DateTime ReadTime
+)
+{
+    private Guid? Uid;
+}
