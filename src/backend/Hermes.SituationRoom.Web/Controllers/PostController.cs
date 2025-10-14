b74833d7d1c73a90fd01df22986c3f08e191fc6a
RavenTamer <jeremias.furrer@hotmail.com>
2025-10-13 20:23:30 -0700

    changed return type in controller to PostWithTagsBo

--- src/backend/Hermes.SituationRoom.Web/Controllers/PostController.cs
+++ src/backend/Hermes.SituationRoom.Web/Controllers/PostController.cs
@@ -19,32 +19,32 @@ public class PostController(IControllerInfrastructure infra, IPostService postSe
 
     [HttpGet("internal/post/activist/by-tags")]
     [SwaggerOperation(Tags = [SwaggerTagDescriptions.ENDPOINT_TAG_INTERNAL_POST])]
-    [ProducesResponseType(typeof(IReadOnlyList<PostBo>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<PostBo>> GetActivistPostsByTags([FromQuery] string tags, [FromQuery] int? limit, [FromQuery] int? offset) =>
+    [ProducesResponseType(typeof(IReadOnlyList<PostWithTagsBo>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PostWithTagsBo>> GetActivistPostsByTags([FromQuery] string tags, [FromQuery] int? limit, [FromQuery] int? offset) =>
         Ok(await postService.GetActivistPostsByTagsAsync(tags, limit ?? 12, offset ?? 0));
 
     [HttpGet("internal/post/journalist/by-tags")]
     [SwaggerOperation(Tags = [SwaggerTagDescriptions.ENDPOINT_TAG_INTERNAL_POST])]
-    [ProducesResponseType(typeof(IReadOnlyList<PostBo>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<PostBo>> GetJournalistPostsByTags([FromQuery] string tags, [FromQuery] int? limit, [FromQuery] int? offset) =>
+    [ProducesResponseType(typeof(IReadOnlyList<PostWithTagsBo>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PostWithTagsBo>> GetJournalistPostsByTags([FromQuery] string tags, [FromQuery] int? limit, [FromQuery] int? offset) =>
         Ok(await postService.GetJournalistPostsByTagsAsync(tags, limit ?? 12, offset ?? 0));
 
     [HttpGet("internal/post/user/{userUid:guid}")]
     [SwaggerOperation(Tags = [SwaggerTagDescriptions.ENDPOINT_TAG_INTERNAL_POST])]
-    [ProducesResponseType(typeof(IReadOnlyList<PostBo>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<PostBo>> GetUserPosts(Guid userUid, [FromQuery] int? limit, [FromQuery] int? offset) =>
+    [ProducesResponseType(typeof(IReadOnlyList<PostWithTagsBo>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PostWithTagsBo>> GetUserPosts(Guid userUid, [FromQuery] int? limit, [FromQuery] int? offset) =>
         Ok(await postService.GetUserPostsAsync(userUid, limit ?? 12, offset ?? 0));
 
     [HttpGet("internal/post/activist")]
     [SwaggerOperation(Tags = [SwaggerTagDescriptions.ENDPOINT_TAG_INTERNAL_POST])]
-    [ProducesResponseType(typeof(IReadOnlyList<PostBo>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<PostBo>> GetAllActivistPosts([FromQuery] int? limit, [FromQuery] int? offset) => 
+    [ProducesResponseType(typeof(IReadOnlyList<PostWithTagsBo>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PostWithTagsBo>> GetAllActivistPosts([FromQuery] int? limit, [FromQuery] int? offset) => 
         Ok(await postService.GetAllActivistPostsAsync(limit ?? 12, offset ?? 0));
 
     [HttpGet("internal/post/journalist")]
     [SwaggerOperation(Tags = [SwaggerTagDescriptions.ENDPOINT_TAG_INTERNAL_POST])]
-    [ProducesResponseType(typeof(IReadOnlyList<PostBo>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<PostBo>> GetAllAJournalistPosts([FromQuery] int? limit, [FromQuery] int? offset) =>
+    [ProducesResponseType(typeof(IReadOnlyList<PostWithTagsBo>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PostWithTagsBo>> GetAllJournalistPosts([FromQuery] int? limit, [FromQuery] int? offset) =>
         Ok(await postService.GetAllJournalistPostsAsync(limit ?? 12, offset ?? 0));
 
     [HttpPost("internal/post/")]
