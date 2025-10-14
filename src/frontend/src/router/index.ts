30a1e2df052433b8e7bd6d94c925db2b7e32144d
Krieger Dario <dario.krieger@icloud.com>
2025-10-13 19:11:43 -0700

    fix: frontend router

--- src/frontend/src/router/index.ts
+++ src/frontend/src/router/index.ts
@@ -84,7 +84,7 @@ const router = createRouter({
     ],
 });
 
-router.beforeEach((to, from, next) => {
+router.beforeEach((to, _, next) => {
     const authStore = useAuthStore();
     const isAuthenticated = authStore.isAuthenticated.value;
 
