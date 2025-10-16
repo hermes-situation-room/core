43d63f7f7ba28c03091f3219a01ccb1a1701f269
RavenTamer <jeremias.furrer@hotmail.com>
2025-10-15 18:55:37 -0700

    list users posts in userprofile

--- src/frontend/src/components/profile-component.vue
+++ src/frontend/src/components/profile-component.vue
@@ -5,6 +5,7 @@ import { services } from '../services/api';
 import type { UserProfileBo } from '../types/user';
 import { useAuthStore } from '../stores/auth-store';
 import { useNotification } from '../composables/use-notification.ts';
+import ProfileUserPostList from './profile-userPostList.vue';
 
 const route = useRoute();
 const router = useRouter();
@@ -227,6 +228,7 @@ watch(() => route.query.id, () => {
                             Login to Message
                         </RouterLink>
                     </div>
+                    <ProfileUserPostList :userProfile="userProfile"></ProfileUserPostList>
                 </div>
             </div>
         </div>
